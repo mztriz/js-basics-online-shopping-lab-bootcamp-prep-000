9e1cf1b755234ac3986d47533337a0941ff81930
Ava Barron <mztriz@gmail.com>
2018-08-21 22:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function total(){
 }
 
 function removeFromCart(item) {
-  console.log(getCart())
+  console.log(typeof getCart())
   if (getCart().indexOf(item) === -1){
     return "That item is not in your cart."
   }

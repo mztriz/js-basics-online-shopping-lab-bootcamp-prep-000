bb7ba49a153f530bc8bbadbf08cf331665dc8385
Ava Barron <mztriz@gmail.com>
2018-08-21 22:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
     return "That item is not in your cart."
   }
   setCart(getCart().splice(getCart().indexOf(item), 1))
-  return getCart()
+  return getCart()  
 }
 
 function placeOrder(cardNumber) {

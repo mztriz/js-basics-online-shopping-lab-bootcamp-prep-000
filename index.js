bf40df1620c8dd1a57e067e76bd7e211abb85ad9
Ava Barron <mztriz@gmail.com>
2018-08-21 22:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
-  let sentance = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  let sentence = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   setCart("")
-  return sentance
+  return sentence
 }
 
 
